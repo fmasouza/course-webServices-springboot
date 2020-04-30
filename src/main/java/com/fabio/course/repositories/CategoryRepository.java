fce578d67a9f4353aa30be5679703e66bde3e53d
fmasouza <fabmacsouza@gmail.com>
2020-04-30 17:37:24 -0300

    Category

---
+++ src/main/java/com/fabio/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.fabio.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fabio.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
