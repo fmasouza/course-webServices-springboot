94160645af4a7d57eeb4eede4285e5911b88d092
fmasouza <fabmacsouza@gmail.com>
2020-04-30 21:13:25 -0300

    Payment, one-to-one association

--- src/main/java/com/fabio/course/config/TestConfig.java
+++ src/main/java/com/fabio/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.fabio.course.entities.Category;
 import com.fabio.course.entities.Order;
 import com.fabio.course.entities.OrderItem;
+import com.fabio.course.entities.Payment;
 import com.fabio.course.entities.Product;
 import com.fabio.course.entities.User;
 import com.fabio.course.entities.enums.OrderStatus;
@@ -83,6 +84,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 		
 	}
 
